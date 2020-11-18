c270c785c4db635188f3f9961939e6a45694fcd5
Grant Pittman <grantdpittman@gmail.com>
2020-11-18 16:10:50 -0600

    adding working filters to seattle test route

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,31 @@
-var tableData = results;
-
-console.log(tableData)
-
-var tbody = d3.select("tbody");
-
 var submitButton = d3.select("#filter-btn");
 
 submitButton.on("click", function() {
     // clears the data of the current table
     
-    tbody.html("");
     // stop the page from refresh
-    d3.event.preventDefault();
+    d3.event.preventDefault()
     // select the "input element" and get the raw html node
-    var inputField = d3.select("#datetime");
-    // get the value property of the "input" element 
-    var inputElement = inputField.property("value");
-    // print value in console
-    var inputTypeArray = tableData.filter(one => one.location === inputElement);  
-    console.log(inputTypeArray);
 
-    
-    // display in the html file (appends it at the end, after displaying all original data)
-    inputTypeArray.forEach((selection) => {
-        // for each "element" create a row
-        var row = tbody.append("tr");
-        //below "object" becomes the targetet array (element)
-        Object.entries(selection).forEach(([key,value]) => {
-            var cell = row.append("td");
-            // adds the "value" to each row in the table
-            cell.text(value);
-        });
-    });      
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("datetime");
+    filter = input.value;
+    console.log(filter)
+    table = document.getElementById("mytable");
+    tr = table.getElementsByTagName("tr");
+    console.log(tr)
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }    
 });
